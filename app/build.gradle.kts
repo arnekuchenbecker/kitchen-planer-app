f487c7fb672c051cb75206a63795ecb174c2a8ef
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2023-11-23 17:44:54 +0100

    Fix API Level

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.scouts.kitchenplaner"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.scouts.kitchenplaner"
