c4e12ca2f5a0cf4aa31854dfeacb8aa35e88a8fe
kittyahy <78612604+kittyahy@users.noreply.github.com>
2023-12-05 11:45:44 +0100

    Add basic navigation classes s.t. they can be connected

---
+++ app/src/main/java/com/scouts/kitchenplaner/ui/navigation/NavGraphProjects.kt
@@ -0,0 +1,29 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023  Arne Kuchenbecker, Antonia Heiming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplaner.ui.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.scouts.kitchenplaner.ui.view.ProjectOverview
+
+fun NavGraphBuilder.projectsNav(navController: NavHostController) {
+    navigation(startDestination = "projectOverview", route = "projects" ){
+        composable("projectOverview"){ ProjectOverview()}
+    }
+}
