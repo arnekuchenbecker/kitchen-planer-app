66776d7666fb450677eb7173f0b2759f93d73b1b
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2023-12-08 15:24:10 +0100

    Add AllergenPicker composable

---
+++ app/src/main/java/com/scouts/kitchenplaner/ui/view/createproject/AllergenCard.kt
@@ -0,0 +1,78 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023  Arne Kuchenbecker, Antonia Heiming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplaner.ui.view.createproject
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.scouts.kitchenplaner.ui.view.CardState
+import com.scouts.kitchenplaner.ui.view.DeleteButton
+import com.scouts.kitchenplaner.ui.view.ExpandableCard
+
+@Composable
+fun AllergenCard(name: String, allergens: List<Pair<String, Boolean>>, onTitleClick: () -> Unit, onDelete: () -> Unit, onItemDelete: (Pair<String, Boolean>) -> Unit, toBeDeleted: Boolean) {
+    var expand by remember { mutableStateOf(false) }
+    ExpandableCard(
+        expanded = expand,
+        onCardArrowClick = { expand = !expand },
+        onTitleClick = onTitleClick,
+        cardState = CardState(name, onDelete, toBeDeleted) {
+            LazyColumn {
+                items(allergens) { (allergen, traces) ->
+                    var itemToBeDeleted by remember { mutableStateOf(false) }
+                    val traceString = if (traces) " (Spuren)" else ""
+                    Row (
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .clickable { itemToBeDeleted = !itemToBeDeleted }
+                                .weight(1.0f),
+                            text = "$allergen$traceString")
+
+                        if (itemToBeDeleted) {
+                            DeleteButton {
+                                onItemDelete(Pair(allergen, traces))
+                                itemToBeDeleted = false
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/scouts/kitchenplaner/ui/view/createproject/AllergenPicker.kt
@@ -0,0 +1,101 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023  Arne Kuchenbecker, Antonia Heiming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplaner.ui.view.createproject
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.scouts.kitchenplaner.ui.view.LazyColumnWrapper
+import com.scouts.kitchenplaner.ui.view.ListEditHeader
+import kotlin.math.min
+
+
+@Composable
+fun AllergenPicker(modifier: Modifier = Modifier, onAdd: (String, String, Boolean) -> Unit, onRemove: (String) -> Unit, onRemoveItem: (String, String, Boolean) -> Unit, allergens: Map<String, List<Pair<String, Boolean>>>) {
+    var displayDialog by remember { mutableStateOf(false) }
+    Column (
+        modifier = modifier.height((90 + 20 * (1 + min(allergens.size, 4))).dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ListEditHeader(
+            onClick = { displayDialog = true },
+            title = "Ernährungsbesonderheiten"
+        )
+
+        LazyColumnWrapper(
+            content = allergens.toList(),
+            DisplayContent = { (name, _), _ ->
+                AllergenListElement(name = name, displayDivider = true)
+            },
+            DisplayLast = { (name, _), _ ->
+                AllergenListElement(name = name, displayDivider = false)
+            },
+            DisplayEmpty = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f),
+                    contentAlignment = Alignment.TopCenter
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(0.dp, 5.dp),
+                        text = "Keine Intoleranten Personen")
+                }
+            }
+        )
+    }
+
+    if (displayDialog) {
+        EditAllergensDialog(
+            onDismissRequest = { displayDialog = false },
+            onAdd = onAdd,
+            onRemove = onRemove,
+            onRemoveItem = onRemoveItem,
+            allergens = allergens
+        )
+    }
+}
+
+@Composable
+fun AllergenListElement(name: String, displayDivider: Boolean) {
+    Column (
+        modifier = Modifier
+            .fillMaxWidth(0.6f),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.padding(0.dp, 5.dp),
+            text = name
+        )
+
+        if (displayDivider) {
+            Divider(thickness = 1.dp)
+        }
+    }
+}
