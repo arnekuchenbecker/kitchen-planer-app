3b489ad5031257b2c4daa26ab4441336b231fcd3
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2023-12-08 15:16:34 +0100

    Update imports

--- app/src/main/java/com/scouts/kitchenplaner/MainActivity.kt
+++ app/src/main/java/com/scouts/kitchenplaner/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.scouts.kitchenplaner.ui.theme.KitchenPlanerTheme
-import com.scouts.kitchenplaner.ui.view.CreateProject
+import com.scouts.kitchenplaner.ui.view.createproject.CreateProject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/scouts/kitchenplaner/ui/view/createproject/CreateProject.kt
@@ -0,0 +1,203 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023  Arne Kuchenbecker, Antonia Heiming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplaner.ui.view
+
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.scouts.kitchenplaner.ui.state.CreateProjectInputState
+import com.scouts.kitchenplaner.ui.viewmodel.CreateProjectViewModel
+import kotlin.math.max
+import kotlin.math.min
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateProject(createProjectViewModel: CreateProjectViewModel = viewModel()) {
+    Scaffold (topBar = {
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            ),
+            title = {
+                Text("Create a New Project")
+            }
+        )
+
+    }, floatingActionButton = {
+        ExtendedFloatingActionButton(onClick = { createProjectViewModel.onProjectCreate() }, icon = {
+            Icon(imageVector = Icons.Filled.Check, contentDescription = "Create project")
+        }, text = { Text("Fertig") })
+    }) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding(it)) {
+            CreateProjectInput(state = createProjectViewModel.inputState.value, modifier = Modifier.align(Alignment.TopCenter))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateProjectInput(state: CreateProjectInputState, modifier: Modifier = Modifier) {
+    Column(modifier = modifier
+        .padding(10.dp)
+        .fillMaxWidth()
+        .verticalScroll(state = rememberScrollState())) {
+        val columnItemModifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 10.dp)
+
+        Row(horizontalArrangement = Arrangement.Center, modifier = columnItemModifier) {
+            val context = LocalContext.current
+            PicturePicker(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .aspectRatio(1.0f),
+                onPathSelected = {
+                    context.contentResolver.takePersistableUriPermission(it!!, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    state.image = it
+                },
+                path = state.image
+            )
+        }
+
+        TextField(
+            value = state.name,
+            onValueChange = { state.name = it },
+            modifier = columnItemModifier
+                .height(70.dp),
+            label = { Text("Project Name") }
+        )
+
+        DockedDatePicker(
+            modifier = columnItemModifier
+                .height(70.dp),
+            dateState = state.startDate,
+            label = "Start-Datum:",
+            displayText = state.startDateString
+        )
+
+        DockedDatePicker(
+            modifier = columnItemModifier
+                .height(70.dp),
+            dateState = state.endDate,
+            label = "End-Datum:",
+            displayText = state.endDateString
+        )
+
+        MealPicker(
+            modifier = columnItemModifier,
+            onAdd = state::addMeal,
+            onRemove = state::removeMeal,
+            meals = state.meals
+        )
+
+        AllergenPicker(
+            modifier = columnItemModifier,
+            onAdd = state::addIntolerantPerson,
+            onRemove = state::removeIntolerantPerson,
+            onRemoveItem = state::removeIntolerancy,
+            allergens = state.allergens
+        )
+
+        Spacer(modifier = Modifier.height(100.dp))
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewCreateProject() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        CreateProject()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewMealPicker() {
+    Surface(modifier = Modifier.height(500.dp)) {
+        EditMealsDialog(onDismissRequest = {}, onAdd = {}, onRemove = {}, meals = listOf("Pizza", "Flammkuchen"))
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun Test() {
+    Text("Test")
+}
