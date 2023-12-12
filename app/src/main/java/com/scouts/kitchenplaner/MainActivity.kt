e0a7509bc51faf9233e5e2f3b9a886f4b1a72674
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2023-12-12 11:17:10 +0100

    Add Hilt to project

--- app/src/main/java/com/scouts/kitchenplaner/MainActivity.kt
+++ app/src/main/java/com/scouts/kitchenplaner/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.scouts.kitchenplaner.ui.theme.KitchenPlanerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
