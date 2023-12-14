9c22601415824d251465d159826325544a357cb6
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2023-12-14 14:27:45 +0100

    Add cat for project insertion

---
+++ app/src/androidTest/java/com/scouts/kitchenplaner/ProjectDatabaseTest.kt
@@ -0,0 +1,68 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023  Arne Kuchenbecker, Antonia Heiming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplaner
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.scouts.kitchenplaner.datalayer.KitchenAppDatabase
+import com.scouts.kitchenplaner.datalayer.daos.ProjectDAO
+import com.scouts.kitchenplaner.datalayer.repositories.ProjectRepository
+import com.scouts.kitchenplaner.model.entities.Project
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class ProjectDatabaseTest {
+
+    private lateinit var repo: ProjectRepository
+    private lateinit var projectDAO: ProjectDAO
+    private lateinit var db: KitchenAppDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, KitchenAppDatabase::class.java).build()
+        projectDAO = db.projectDao()
+        repo = ProjectRepository(projectDAO)
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun testInsertProject() : Unit = runBlocking {
+        val project = Project(name = "Test")
+        repo.insertProject(project)
+        val retValue = repo.getProject("Test")
+        assertEquals("Test", retValue.name)
+        assertNotEquals(null, retValue.id)
+    }
+}
