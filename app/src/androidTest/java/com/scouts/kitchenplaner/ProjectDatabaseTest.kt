04786a226caaec7cda1ecc9cb4a71c4bb29321b6
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2023-12-15 08:21:52 +0100

    Fix unit test

--- app/src/androidTest/java/com/scouts/kitchenplaner/ProjectDatabaseTest.kt
+++ app/src/androidTest/java/com/scouts/kitchenplaner/ProjectDatabaseTest.kt
@@ -20,14 +20,17 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import app.cash.turbine.test
 import com.scouts.kitchenplaner.datalayer.KitchenAppDatabase
 import com.scouts.kitchenplaner.datalayer.daos.ProjectDAO
 import com.scouts.kitchenplaner.datalayer.repositories.ProjectRepository
 import com.scouts.kitchenplaner.model.entities.Project
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -44,7 +47,8 @@ class ProjectDatabaseTest {
     fun createDb() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(
-            context, KitchenAppDatabase::class.java).build()
+            context, KitchenAppDatabase::class.java
+        ).build()
         projectDAO = db.projectDao()
         repo = ProjectRepository(projectDAO)
     }
@@ -58,11 +62,40 @@ class ProjectDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun testInsertProject() : Unit = runBlocking {
+    fun testInsertProject(): Unit = runBlocking {
         val project = Project(name = "Test")
         repo.insertProject(project)
         val retValue = repo.getProject("Test")
         assertEquals("Test", retValue.name)
         assertNotEquals(null, retValue.id)
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun testGetAllProjects(): Unit = runTest {
+        val project = Project(name = "Test")
+        val project2 = Project(name = "Test2")
+
+        repo.getAllProjectsOverview().test {
+            val projectId = repo.insertProject(project)
+            val flowContent = awaitItem()
+
+            assertEquals("Incorrect number of projects in flow", 1, flowContent.size)
+            assertEquals("Incorrect project name", project.name, flowContent[0].name)
+            assertEquals("Incorrect project id", projectId, flowContent[0].id)
+
+            val project2Id = repo.insertProject(project2)
+
+            assertNotEquals("Same project id for different projects", projectId, project2Id)
+
+            val flowContent2 = awaitItem()
+
+            assertEquals("Incorrect number of projects in flow", 2, flowContent2.size)
+            assertTrue("No project with id $projectId found", flowContent2.any { it.id == projectId })
+            assertTrue("No project with id $project2Id found", flowContent2.any { it.id == project2Id })
+            assertTrue("No project with name ${project.name} found", flowContent2.any { it.name == project.name })
+            assertTrue("No project with name ${project2.name} found", flowContent2.any { it.name == project2.name })
+        }
     }
 }
