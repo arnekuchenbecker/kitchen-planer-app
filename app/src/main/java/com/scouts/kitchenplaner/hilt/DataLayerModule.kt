fe56dafe1e99161103d8af1b3e7791483b6982f2
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2023-12-19 13:55:04 +0100

    Add Provides Methods for DataLayerClasses

---
+++ app/src/main/java/com/scouts/kitchenplaner/hilt/DataLayerModule.kt
@@ -0,0 +1,46 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023  Arne Kuchenbecker, Antonia Heiming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplaner.hilt
+
+import android.content.Context
+import androidx.room.Room
+import com.scouts.kitchenplaner.datalayer.KitchenAppDatabase
+import com.scouts.kitchenplaner.datalayer.daos.ProjectDAO
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DataLayerModule {
+    @Provides
+    fun projectDao(database: KitchenAppDatabase) : ProjectDAO {
+        return database.projectDao()
+    }
+
+    @Provides
+    @Singleton
+    fun myDatabase(@ApplicationContext application: Context): KitchenAppDatabase {
+        return Room
+            .databaseBuilder(application, KitchenAppDatabase::class.java, "hordentopf.db")
+            .build()
+    }
+}
