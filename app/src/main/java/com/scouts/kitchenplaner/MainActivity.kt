d524e5f803fe80f817226f10e573a231a753d994
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-01-08 15:39:12 +0100

    Dummy Commit 2

--- app/src/main/java/com/scouts/kitchenplaner/MainActivity.kt
+++ app/src/main/java/com/scouts/kitchenplaner/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KitchenPlanerTheme(dynamicColor = true) {
+            KitchenPlanerTheme(dynamicColor = false) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
