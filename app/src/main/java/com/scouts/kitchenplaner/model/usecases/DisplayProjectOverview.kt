427ba320b99ce07f3071c0d110074b4e2522d010
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-01-16 10:17:44 +0100

    Move Project flow combination to the domain layer to allow for partial updates

--- app/src/main/java/com/scouts/kitchenplaner/model/usecases/DisplayProjectOverview.kt
+++ app/src/main/java/com/scouts/kitchenplaner/model/usecases/DisplayProjectOverview.kt
@@ -16,18 +16,91 @@
 
 package com.scouts.kitchenplaner.model.usecases
 
+import com.scouts.kitchenplaner.datalayer.repositories.AllergenRepository
 import com.scouts.kitchenplaner.datalayer.repositories.ProjectRepository
+import com.scouts.kitchenplaner.datalayer.repositories.RecipeManagementRepository
+import com.scouts.kitchenplaner.datalayer.repositories.RecipeRepository
+import com.scouts.kitchenplaner.model.DomainLayerRestricted
+import com.scouts.kitchenplaner.model.entities.MealSlot
 import com.scouts.kitchenplaner.model.entities.Project
+import com.scouts.kitchenplaner.model.entities.RecipeStub
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 class DisplayProjectOverview @Inject constructor(
-    private val projectRepository: ProjectRepository
+    private val projectRepository: ProjectRepository,
+    private val allergenRepository: AllergenRepository,
+    private val recipeManagementRepository: RecipeManagementRepository,
+    private val recipeRepository: RecipeRepository
 ) {
+    @OptIn(DomainLayerRestricted::class)
     fun getProject(projectId: Long) : Flow<Project> {
-        return projectRepository.getProjectByID(projectId)
+        val initialProject = runBlocking {
+            projectRepository.getProjectMetaDataByID(projectId).map {
+                Project(
+                    it.stub.id,
+                    it.stub.name,
+                    it.startDate,
+                    it.endDate,
+                    mutableListOf(),
+                    mutableListOf(),
+                    it.stub.imageUri
+                )
+            }.first()
+        }
+
+        val metaDataFlow = projectRepository.getProjectMetaDataByID(projectId)
+        val mealFlow = projectRepository.getMealsByProjectID(projectId)
+        val allergenPersonFlow = allergenRepository.getAllergenPersonsByProjectID(projectId)
+        val mealSlotFlow = recipeManagementRepository.getRecipeMealSlotsForProject(projectId)
+        val recipeFlow = recipeRepository.getRecipeStubsByProjectId(projectId)
+        val mealPlanFlow = mealSlotFlow.combine(recipeFlow) { meals, recipes ->
+            val mealMap = mutableMapOf<MealSlot, RecipeStub>()
+            meals.forEach { (slot, recipeId) ->
+                val recipe = recipes.find { it.id == recipeId }
+                if (recipe != null) {
+                    mealMap[slot] = recipe
+                }
+            }
+            mealMap
+        }
+        val numberChangeFlow = projectRepository.getPersonNumberChangesByProjectID(projectId)
+
+        return flowOf(initialProject)
+            .combine(metaDataFlow) { project, metaData ->
+                project.setID(metaData.stub.id)
+                project.setName(metaData.stub.name)
+                project.setStartDate(metaData.startDate)
+                project.setEndDate(metaData.endDate)
+                project.setImageUri(metaData.stub.imageUri)
+                project
+            }
+            .combine(allergenPersonFlow) { project, allergenPersons ->
+                project.setAllergenPersons(allergenPersons)
+                project
+            }
+            .combine(mealFlow) { project, meals ->
+                project.mealPlan.setMeals(meals)
+                project
+            }
+            .combine(mealPlanFlow) { project, mealPlan ->
+                project.mealPlan.setPlan(mealPlan)
+                project
+            }
+            .combine(numberChangeFlow) { project, numberChanges ->
+                project.mealPlan.setNumberChanges(numberChanges)
+                project
+            }
     }
 
+    suspend fun archiveProject(project: Project) {
+
+    }
     //TODO archiveProjectWithID - missing DataLayer support
     //TODO leaveProjectWithID - missing DataLayer support
 }
