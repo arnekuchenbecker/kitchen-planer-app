ed6d490dc948ba5c1cdf0aebaed13822c96ea4c6
kittyahy <78612604+kittyahy@users.noreply.github.com>
2024-01-19 10:48:04 +0100

    pull out project field

--- app/src/main/java/com/scouts/kitchenplaner/ui/view/projectoverview/ProjectOverview.kt
+++ app/src/main/java/com/scouts/kitchenplaner/ui/view/projectoverview/ProjectOverview.kt
@@ -16,25 +16,15 @@
 
 package com.scouts.kitchenplaner.ui.view.projectoverview
 
-import android.net.Uri
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.HideImage
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -47,13 +37,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImage
 import com.scouts.kitchenplaner.ui.view.LazyColumnWrapper
 import com.scouts.kitchenplaner.ui.viewmodel.ProjectSelectionViewModel
 
@@ -119,76 +105,21 @@ fun ProjectOverview(
             LazyColumnWrapper(modifier = Modifier.padding(10.dp),
                 content = projects,
                 DisplayContent = { (project, selected), _ ->
-                    Box(modifier = Modifier
-                        .clickable {
-                            if (!viewModel.archive) {
-                                onNavigateToDetailedProject(
-                                    project.id
-                                )
-                            } else {
-                                viewModel.toggleSelection(
-                                    projectId = project.id, selected = !selected
-                                )
-                                println("Project ${project.id} is selected = $selected")
-                            }
-                        }
-                        .fillMaxWidth()
-                        .padding(5.dp)
-                        .border(
-                            width = 2.dp,
-                            color = MaterialTheme.colorScheme.primary,
-                            shape = RoundedCornerShape(6.dp)
-                        )
-                        .height(75.dp)
-
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .padding(start = 5.dp, end = 15.dp)
-                        ) {
-                            if (project.imageUri == Uri.EMPTY) {
-                                Icon(
-                                    modifier = Modifier
-                                        .fillMaxHeight(0.9f)
-                                        .aspectRatio(1.0f)
-                                        .padding(start = 5.dp),
-                                    imageVector = Icons.Filled.HideImage,
-                                    contentDescription = "Projektplatzhalter"
-                                )
-                            } else {
-                                AsyncImage(
-                                    model = project.imageUri,
-                                    contentDescription = "Project Image",
-                                    modifier = Modifier
-                                        .fillMaxHeight(0.85f)
-                                        .aspectRatio(1.0f)
-                                        .clip(CircleShape),
-                                    contentScale = ContentScale.Crop,
-                                )
-                            }
-                            Text(
-                                modifier = Modifier.padding(10.dp),
-                                text = project.name,
-                                color = MaterialTheme.colorScheme.onSecondaryContainer
-                            )
-                            println("selected: $selected")
-                            if (viewModel.archive && selected) {
-                                Spacer(modifier = Modifier.weight(1f))
-                                Icon(
-                                    imageVector = Icons.Filled.Delete,
-                                    contentDescription = "zu löschendes projekt",
-                                    tint = MaterialTheme.colorScheme.error,
-                                    modifier = Modifier
-                                        .fillMaxHeight(0.7f)
-                                        .aspectRatio(1.0f)
-
-                                )
-                            }
-                        }
-
-                    }
+                    projectField(
+                        project = project,
+                        selected = selected,
+                        viewModel = viewModel,
+                        onNavigateToDetailedProject = onNavigateToDetailedProject
+                    )
+                }, DisplayLast = { (project, selected), _ ->
+                    projectField(
+                        project = project,
+                        selected = selected,
+                        viewModel = viewModel,
+                        onNavigateToDetailedProject = onNavigateToDetailedProject
+                    )
+                    //To allow scrolling stuff from behind the FAB
+                    Spacer(modifier = Modifier.height(75.dp))
                 },
                 DisplayEmpty = {
                     Text("${projects.size}")
@@ -203,6 +134,3 @@ fun ProjectOverview(
         }
     }
 }
-
-
-
