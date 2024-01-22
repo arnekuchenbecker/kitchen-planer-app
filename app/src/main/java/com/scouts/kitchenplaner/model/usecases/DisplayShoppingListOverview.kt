a56f5834f52c643c9bedd1c08a7c102c7c5bc97c
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-01-22 16:41:55 +0100

    Add usecase for getting shopping lists for a project

---
+++ app/src/main/java/com/scouts/kitchenplaner/model/usecases/DisplayShoppingListOverview.kt
@@ -0,0 +1,36 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023-2024 Arne Kuchenbecker, Antonia Heiming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplaner.model.usecases
+
+import com.scouts.kitchenplaner.datalayer.repositories.ShoppingListRepository
+import com.scouts.kitchenplaner.model.entities.Project
+import com.scouts.kitchenplaner.model.entities.ShoppingList
+import com.scouts.kitchenplaner.model.entities.ShoppingListStub
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class DisplayShoppingListOverview @Inject constructor(
+    private val shoppingListRepository: ShoppingListRepository
+) {
+    fun getAllShoppingListsForProject(project: Project) : Flow<List<ShoppingList>> {
+        return shoppingListRepository.getShoppingListsForProject(project.id ?: 0)
+    }
+
+    fun getShoppingListStubsForProject(project: Project) : Flow<List<ShoppingListStub>> {
+        return shoppingListRepository.getShoppingListStubsForProject(project.id ?: 0)
+    }
+}
