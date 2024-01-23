995103e348bf817fe503530530cb935593978669
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-01-23 14:52:27 +0100

    Add viewmodels for the frame and the overview section for project details

---
+++ app/src/main/java/com/scouts/kitchenplaner/ui/viewmodel/ProjectFrameViewModel.kt
@@ -0,0 +1,30 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023-2024 Arne Kuchenbecker, Antonia Heiming
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplaner.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class ProjectFrameViewModel @Inject constructor() : ViewModel() {
+    fun getProjectName(projectId: Long) : StateFlow<String> {
+        return MutableStateFlow("Dummy Project")
+    }
+}
