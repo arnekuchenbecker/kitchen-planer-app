46ca7c00895e428dc1a1922697dbe489618a871c
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-02-06 11:12:01 +0100

    Extract UI Elements for invitation to its own file

--- app/src/main/java/com/scouts/kitchenplaner/ui/view/invitetoproject/InviteToProject.kt
+++ app/src/main/java/com/scouts/kitchenplaner/ui/view/invitetoproject/InviteToProject.kt
@@ -16,16 +16,10 @@
 
 package com.scouts.kitchenplaner.ui.view.invitetoproject
 
-import android.content.Intent
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -42,25 +36,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ClipboardManager
-import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import com.scouts.kitchenplaner.ui.theme.KitchenPlanerTheme
+import com.scouts.kitchenplaner.ui.view.InvitationElements
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InviteToProject(projectId: Long, onNavigateToProject: () -> Unit) {
     var invitePeople by remember { mutableStateOf(false) }
-
-    val link = "http://joinproject.app?id=$projectId"
-    val context = LocalContext.current
-    val clipboardManager: ClipboardManager = LocalClipboardManager.current
 
     Scaffold (
         topBar = {
@@ -89,43 +74,7 @@ fun InviteToProject(projectId: Long, onNavigateToProject: () -> Unit) {
             modifier = Modifier.padding(it)
         ) {
             if (invitePeople) {
-                Text(
-                    modifier = Modifier.padding(10.dp),
-                    fontSize = 20.sp,
-                    text = "Share this link with other people you want to invite to the project:")
-
-                Box (
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .border(3.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(5.dp))
-                        .clickable {
-                            clipboardManager.setText(AnnotatedString(link))
-                        }
-                ) {
-                    Text(
-                        modifier = Modifier
-                            .padding(8.dp),
-                        text = link
-                    )
-                }
-
-                Button(onClick = {
-                    val intent = Intent(Intent.ACTION_SEND).apply {
-                        type = "text/plain"
-                        putExtra(Intent.EXTRA_TEXT, link)
-                    }
-                    ContextCompat.startActivity(context, Intent.createChooser(intent, "Share link"), null)
-
-
-                }) {
-                    Icon(
-                        imageVector = Icons.Filled.Share,
-                        contentDescription = "Invite other people",
-                        modifier = Modifier.padding(end = 10.dp)
-                    )
-
-                    Text("Projekt teilen")
-                }
+                InvitationElements(projectId = projectId)
             } else {
                 Text(
                     modifier = Modifier.padding(5.dp),
