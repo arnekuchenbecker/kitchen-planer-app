68e5d68c63b06442ccb427c128bcfc8dad263aaf
kittyahy <78612604+kittyahy@users.noreply.github.com>
2024-02-14 14:22:24 +0100

    Add header to start screen

--- app/src/main/java/com/scouts/kitchenplaner/ui/view/StartScreen.kt
+++ app/src/main/java/com/scouts/kitchenplaner/ui/view/StartScreen.kt
@@ -19,7 +19,12 @@ package com.scouts.kitchenplaner.ui.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -39,40 +44,52 @@ fun StartScreen(
 ) {
     var projectId by remember { mutableStateOf(0f) }
     var recipeID by remember { mutableStateOf(0f) }
-    Column(modifier = modifier) {
-        Text(text = "This is the start screen, where recipes etc are displayed")
-        Text(text = "available Links to other sides are: ")
-        Row {
-            Text("ProjectDetails")
-            Slider(
-                modifier = Modifier.fillMaxWidth(0.3f),
-                value = projectId,
-                onValueChange = { projectId = it },
-                valueRange = 1f..10f,
-                steps = 1
-            )
-            Button(onClick = { onNavigateToDetailedProject(projectId.toLong()) }) {}
 
-        }
-        Row {
-            Text("ProjectCreation")
-            Button(onClick = onNavigateToProjectCreation) {}
-        }
-        Row {
-            Text("RecipeCreation")
-            Button(onClick = onNavigateToCreateRecipe) {}
-        }
-        Row {
-            Text("RecipeDetails")
-            Slider(
-                modifier = Modifier.fillMaxWidth(0.3f),
-                value = recipeID,
-                onValueChange = { recipeID = it },
-                valueRange = 1f..15f,
-                steps = 15
+    Scaffold(topBar = {
+        HeaderWithButton(
+            title = "Übersicht",
+            buttonClick = { /*Logout*/ }
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.Logout,
+                contentDescription = "Logout button"
             )
-            Button(onClick = { onNavigateToRecipeDetail(recipeID.toLong()) }) {
+        }
+    }) {
+        Column(modifier = modifier.padding(it)) {
+            Text(text = "This is the start screen, where recipes etc are displayed")
+            Text(text = "available Links to other sides are: ")
+            Row {
+                Text("ProjectDetails")
+                Slider(
+                    modifier = Modifier.fillMaxWidth(0.3f),
+                    value = projectId,
+                    onValueChange = { projectId = it },
+                    valueRange = 1f..10f,
+                    steps = 1
+                )
+                Button(onClick = { onNavigateToDetailedProject(projectId.toLong()) }) {}
+            }
+            Row {
+                Text("ProjectCreation")
+                Button(onClick = onNavigateToProjectCreation) {}
+            }
+            Row {
+                Text("RecipeCreation")
+                Button(onClick = onNavigateToCreateRecipe) {}
+            }
+            Row {
+                Text("RecipeDetails")
+                Slider(
+                    modifier = Modifier.fillMaxWidth(0.3f),
+                    value = recipeID,
+                    onValueChange = { recipeID = it },
+                    valueRange = 1f..15f,
+                    steps = 15
+                )
+                Button(onClick = { onNavigateToRecipeDetail(recipeID.toLong()) }) {
 
+                }
             }
         }
     }
