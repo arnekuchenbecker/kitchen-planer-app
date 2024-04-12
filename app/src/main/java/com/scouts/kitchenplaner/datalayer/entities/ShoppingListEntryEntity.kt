428e9c978ce2fae4803fada9d702ad01a50e741d
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-04-12 15:59:02 +0200

    Edit entities to allow for dynamic shopping lists

--- app/src/main/java/com/scouts/kitchenplaner/datalayer/entities/ShoppingListEntryEntity.kt
+++ app/src/main/java/com/scouts/kitchenplaner/datalayer/entities/ShoppingListEntryEntity.kt
@@ -19,23 +19,45 @@ package com.scouts.kitchenplaner.datalayer.entities
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
+import java.util.Date
 
+/**
+ * Database Entity for entries of shopping lists. Shopping lists update dynamically if referenced
+ * recipes are updated. To facilitate this, no fixed values are stored, but instead the MealSlot for
+ * which this entry includes an ingredient.
+ *
+ * @param listId The shopping list this entry belongs to
+ * @param projectId The project the shopping list belongs to
+ * @param mealDate The date the meal is cooked on for which this ingredient is required
+ * @param meal The meal for which this ingredient is required
+ * @param ingredientName The name of the ingredient
+ */
 @Entity(
     tableName = "shoppingListEntries",
-    primaryKeys = ["listId", "itemName"],
+    primaryKeys = ["listId", "mealDate", "meal", "ingredientName"],
     foreignKeys = [
         ForeignKey(
             entity = ShoppingListEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["listId"],
+            parentColumns = ["id", "projectId"],
+            childColumns = ["listId", "projectId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = MealEntity::class,
+            parentColumns = ["projectId", "name"],
+            childColumns = ["projectId", "meal"],
             onDelete = ForeignKey.CASCADE
         )
     ],
-    indices = [Index("listId")]
+    indices = [
+        Index("listId", "projectId"),
+        Index("projectId", "meal")
+    ]
 )
 data class ShoppingListEntryEntity(
     var listId: Long,
-    val itemName: String,
-    val amount: Int,
-    val unit: String
+    val projectId: Long,
+    val mealDate: Date,
+    val meal: String,
+    val ingredientName: String
 )
