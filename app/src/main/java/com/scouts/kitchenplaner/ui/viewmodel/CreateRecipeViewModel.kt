c496e9a11201c61edcc36e44fdbe4b328ce24187
kittyahy <78612604+kittyahy@users.noreply.github.com>
2024-04-15 16:56:19 +0200

    kotlinDoc for createRecipeViewModel

--- app/src/main/java/com/scouts/kitchenplaner/ui/viewmodel/CreateRecipeViewModel.kt
+++ app/src/main/java/com/scouts/kitchenplaner/ui/viewmodel/CreateRecipeViewModel.kt
@@ -38,10 +38,13 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+/**
+ * View model that contains all information which should be stored when creating a new recipe and creates a new recipe.
+ * It also handles the import from chefkoch recipes.
+ */
 @HiltViewModel
 class CreateRecipeViewModel @Inject constructor(
-    private val createRecipe: CreateRecipe,
-    private val importRecipe: ImportRecipe
+    private val createRecipe: CreateRecipe, private val importRecipe: ImportRecipe
 ) : ViewModel() {
     private val idRegex = Regex("^[0-9]+$")
     var recipeName by mutableStateOf("")
@@ -60,16 +63,16 @@ class CreateRecipeViewModel @Inject constructor(
 
     val navigateFlow = MutableStateFlow<Long?>(null)
 
+    /**
+     * Creates a recipe and stores it. It also does some sanity checks.
+     * Note that the recipe creation is going to be in a coroutine.
+     */
     fun createRecipe() {
-        if (recipeName.isBlank()
-            || ingredients.isEmpty()
-            || ingredients.any { (_, ingredients) -> ingredients.isEmpty() }
-        ) {
+        if (recipeName.isBlank() || ingredients.isEmpty() || ingredients.any { (_, ingredients) -> ingredients.isEmpty() }) {
             return
         }
         viewModelScope.launch {
-            val recipe = Recipe(
-                name = recipeName,
+            val recipe = Recipe(name = recipeName,
                 imageURI = uri ?: Uri.EMPTY,
                 description = description,
                 numberOfPeople = calculatedFor.toIntOrNull() ?: 1,
@@ -79,35 +82,54 @@ class CreateRecipeViewModel @Inject constructor(
                 instructions = instructions,
                 ingredientGroups = ingredients.map { (name, ingredients) ->
                     IngredientGroup(name, ingredients)
-                }
-            )
+                })
             val recipeId = createRecipe.createRecipe(recipe)
             navigateFlow.emit(recipeId)
         }
     }
 
+    /**
+     * Adds a new ingredient to a already existing ingredient group.
+     * @param group The name of the ingredient group where the ingredient should added
+     * @param ingredient The ingredient which should be added
+     */
     fun addIngredient(group: String, ingredient: Ingredient) {
         if (ingredient.name.isNotBlank() && ingredient.amount != 0f && ingredient.unit.isNotBlank()) {
             _ingredients[group]?.add(ingredient)
         }
     }
 
+    /**
+     * Adds a new empty ingredient group
+     * @param group the name of the ingredient group that should be created.
+     */
     fun addIngredientGroup(group: String) {
         if (!_ingredients.containsKey(group)) {
             _ingredients[group] = mutableStateListOf()
         }
     }
 
+    /**
+     * deletes all occurrences of the ingredient in a ingredient group
+     *
+     * @param group the name of the ingredient group from where the ingredient should be deleted
+     * @param ingredient the ingredient that should be deleted.
+     */
     fun deleteIngredient(group: String, ingredient: Ingredient) {
         _ingredients[group]?.removeAll { it.name == ingredient.name }
     }
 
+    /**
+     * Deletes an ingredient group
+     * @param group the name of the ingredient group that should be deleted
+     */
     fun deleteGroup(group: String) {
         _ingredients.remove(group)
     }
 
     /**
-     * Source can either be the full chefkoch URL or the recipe ID only
+     * Imports a chefkoch recipe from the source
+     * @param source Source can either be the full chefkoch URL or the recipe ID only
      */
     fun importRecipe(source: String) {
         if (idRegex.matches(source)) {
@@ -125,13 +147,9 @@ class CreateRecipeViewModel @Inject constructor(
 
     private fun importRecipeFromID(id: Long) {
         viewModelScope.launch {
-            importRecipe.import(
-                id = id,
-                onFailure = { code, message ->
-                    println("Request failed with code $code: $message")
-                },
-                onSuccess = { setValues(it) }
-            )
+            importRecipe.import(id = id, onFailure = { code, message ->
+                println("Request failed with code $code: $message")
+            }, onSuccess = { setValues(it) })
         }
     }
 
@@ -142,10 +160,8 @@ class CreateRecipeViewModel @Inject constructor(
         calculatedFor = recipe.numberOfPeople.toString()
         allergenState = RecipeAllergenState()
         _instructions = mutableStateListOf<String>().apply { addAll(recipe.instructions) }
-        _ingredients = recipe.ingredientGroups
-            .map { group ->
+        _ingredients = recipe.ingredientGroups.map { group ->
                 Pair(group.name, group.ingredients.toMutableStateList())
-            }
-            .toMutableStateMap()
+            }.toMutableStateMap()
     }
 }
