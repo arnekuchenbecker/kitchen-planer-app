7fe1f53db119533e97657492029f385ae0552b85
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-05-14 08:48:42 +0200

    Start building the Recipe for Cooking screen

--- app/src/main/java/com/scouts/kitchenplaner/ui/view/recipeforproject/RecipeForProject.kt
+++ app/src/main/java/com/scouts/kitchenplaner/ui/view/recipeforproject/RecipeForProject.kt
@@ -19,14 +19,71 @@ package com.scouts.kitchenplaner.ui.view.recipeforproject
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.scouts.kitchenplaner.model.entities.Ingredient
+import com.scouts.kitchenplaner.model.entities.IngredientGroup
+import com.scouts.kitchenplaner.model.entities.MealSlot
+import com.scouts.kitchenplaner.model.entities.Project
+import com.scouts.kitchenplaner.ui.view.recipes.IngredientsInput
+import com.scouts.kitchenplaner.ui.viewmodel.RecipeForCookingViewModel
 
 @Composable
-fun RecipeForProjectScreen(recipeID: Long) {
-    Column {
-        Text("On this screen you can see a recipe for cooking while you are in a project")
-        Text("Recipe $recipeID is shown", color = Color.Red)
-        Text(text = "available Links to other sides are: ")
+fun RecipeForProjectScreen(
+    project: Project,
+    mealSlot: MealSlot,
+    recipeID: Long,
+    onNavigateToAlternative: (Long) -> Unit,
+    onNavigateToRecipeDetails: (Long) -> Unit,
+    recipeViewModel: RecipeForCookingViewModel = hiltViewModel()
+) {
+    var recipeInitialized by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = null) {
+        recipeViewModel.getRecipe(project, mealSlot, recipeID)
+        recipeInitialized = true
     }
 
+    if (recipeInitialized) {
+        val recipeForCooking by recipeViewModel.recipeForCooking.collectAsState()
+
+        Column {
+            Text(text = recipeForCooking.name)
+
+            Text(text = "Anzahl Personen: ${recipeForCooking.people}")
+
+            IngredientsInput(ingredientGroups = recipeForCooking.ingredientGroups, editable = false)
+
+            //DisplayInstructions(recipeForCooking.instructions)
+
+            //DisplayAlternatives(recipeForCooking.alternatives)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DisplayIngredientGroupPreview() {
+    val group1 = IngredientGroup(
+        "Test",
+        listOf(
+            Ingredient("Mais", 1.0, "kg"),
+            Ingredient("Brot", 2.0, "kg"),
+            Ingredient("Salat", 3.0, "Köpfe")
+        )
+    )
+    val group2 = IngredientGroup(
+        "Zutaten",
+        listOf(
+            Ingredient("Spinat", 500.0, "g"),
+            Ingredient("Gute Laune", 1.0, "Stk")
+        )
+    )
+    IngredientsInput(ingredientGroups = listOf(group1, group2), editable = false)
 }
