5bdcac02e0879dbb7357826e0650ed176f19ea82
kittyahy <78612604+kittyahy@users.noreply.github.com>
2024-06-10 16:27:00 +0200

    first version for recipe views

--- app/src/main/java/com/scouts/kitchenplaner/ui/view/recipedetails/RecipeDetails.kt
+++ app/src/main/java/com/scouts/kitchenplaner/ui/view/recipedetails/RecipeDetails.kt
@@ -16,21 +16,72 @@
 
 package com.scouts.kitchenplaner.ui.view.recipedetails
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.scouts.kitchenplaner.ui.view.ContentBox
+import com.scouts.kitchenplaner.ui.view.Header
+import com.scouts.kitchenplaner.ui.view.Headline
+import com.scouts.kitchenplaner.ui.view.PicturePicker
+import com.scouts.kitchenplaner.ui.viewmodel.EditRecipeViewModel
 
 
 @Composable
-fun RecipeDetails(recipeID: Long) {
+fun RecipeDetails(
+    recipeID: Long, viewModel: EditRecipeViewModel = hiltViewModel()
+) {
 
-    Column {
-        Text("On this screen you can see a detailed recipe")
-        Text(text = "The recipeID is $recipeID", color = Color.Red)
+    var recipeInitialized by remember { mutableStateOf(false) }
 
-        Text(text = "available Links to other sides are: ")
-
+    LaunchedEffect(key1 = null) {
+        viewModel.getRecipe(recipeID)
+        recipeInitialized = true
     }
+    if (recipeInitialized) {
+        val recipe by viewModel.recipeFlow.collectAsState()
 
+        Column {
+            Header(title = recipe.name)
+            Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                ContentBox(title = "Zutaten", modifier = Modifier.fillMaxWidth(0.45f)) {
+                    recipe.ingredientGroups.forEach { group ->
+                        Headline(group.name)
+                        Column {
+                            group.ingredients.forEach {
+                                Row {
+                                    Text(it.name)
+                                    Text(it.amount.toString())
+                                    Text(it.unit)
+                                }
+                            }
+                        }
+                    }
+                }
+                PicturePicker(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .aspectRatio(1f),
+                    onPathSelected = {},
+                    path = recipe.imageURI
+                )
+            }
+        }
+        ContentBox(title = "Schritte") {
+            //recipe.instructions.forEachIndexed()
+
+        }
+    }
 }
