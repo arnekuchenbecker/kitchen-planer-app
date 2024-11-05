f710d431de902b4ddd177e843be252ea1eb0d8f5
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2024-11-05 14:58:03 +0100

    Add equals and hashcode implementations

--- app/src/main/java/com/scouts/kitchenplaner/model/entities/UnitConversion.kt
+++ app/src/main/java/com/scouts/kitchenplaner/model/entities/UnitConversion.kt
@@ -17,27 +17,28 @@
 package com.scouts.kitchenplaner.model.entities
 
 import com.scouts.kitchenplaner.datalayer.entities.UnitConversionEntity
+import com.scouts.kitchenplaner.model.DomainLayerRestricted
 
 /**
  * Entity representing a possible Unit Conversion. To apply a Unit Conversion to an ingredient, the
  * ingredient's name has to match [ingredientMatcher] and the ingredient's unit has to be the same
- * as [sourceUnit]
+ * as [_sourceUnit]
  *
  * @param ingredientMatcher Regex identifying what ingredients this UnitConversion can be applied to
- * @param sourceUnit The unit this UnitConversion converts from
- * @param destinationUnit The unit this UnitConversion converts to
- * @param factor The factor to apply to an ingredient to convert from [sourceUnit] to
- * [destinationUnit]
+ * @param _sourceUnit The unit this UnitConversion converts from
+ * @param _destinationUnit The unit this UnitConversion converts to
+ * @param factor The factor to apply to an ingredient to convert from [_sourceUnit] to
+ * [_destinationUnit]
  */
 class UnitConversion(
     private val ingredientMatcher: Regex,
-    private val sourceUnit: String,
-    private val destinationUnit: String,
+    private val _sourceUnit: String,
+    private val _destinationUnit: String,
     private val factor: Double
 ) {
     /**
      * Applies this UnitConversion to the given ingredient, returning an Ingredient with the unit
-     * [destinationUnit] by multiplying the ingredient's amount by [factor] if this UnitConversion
+     * [_destinationUnit] by multiplying the ingredient's amount by [factor] if this UnitConversion
      * is applicable to the Ingredient (see [isApplicable]) or null otherwise
      *
      * @param ingredient The ingredient to which to apply this UnitConversion
@@ -47,7 +48,7 @@ class UnitConversion(
      */
     fun apply(ingredient: Ingredient): Ingredient? {
         return if (isApplicable(ingredient)) {
-            Ingredient(ingredient.name, ingredient.amount * factor, destinationUnit)
+            Ingredient(ingredient.name, ingredient.amount * factor, _destinationUnit)
         } else {
             null
         }
@@ -56,14 +57,14 @@ class UnitConversion(
     /**
      * Checks whether thisUnitConversion is applicable to the given Ingredient, i.e. if the
      * [ingredientMatcher] matches the ingredient's name and the ingredient's unit is equal to
-     * [sourceUnit]
+     * [_sourceUnit]
      *
      * @param ingredient The ingredient to check whether this UnitConversion is applicable to
      *
      * @return Whether this UnitConversion is applicable to the given Ingredient
      */
     fun isApplicable(ingredient: Ingredient): Boolean {
-        return ingredientMatcher.matches(ingredient.name) && ingredient.unit == sourceUnit
+        return ingredientMatcher.matches(ingredient.name) && ingredient.unit == _sourceUnit
     }
 
     /**
@@ -77,9 +78,41 @@ class UnitConversion(
         return UnitConversionEntity(
             projectID,
             ingredientMatcher.pattern,
-            sourceUnit,
-            destinationUnit,
+            _sourceUnit,
+            _destinationUnit,
             factor
         )
+    }
+
+    @DomainLayerRestricted
+    val pattern: String
+        get() = ingredientMatcher.pattern
+
+    @DomainLayerRestricted
+    val sourceUnit: String
+        get() = _sourceUnit
+
+    @DomainLayerRestricted
+    val destinationUnit: String
+        get() = _destinationUnit
+
+    @OptIn(DomainLayerRestricted::class)
+    override fun toString(): String {
+        return "($pattern: [$sourceUnit -> $destinationUnit])"
+    }
+
+    override fun hashCode(): Int {
+        return ingredientMatcher.pattern.hashCode() + _sourceUnit.hashCode() + _destinationUnit.hashCode() + factor.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is UnitConversion) {
+            ingredientMatcher.pattern == other.ingredientMatcher.pattern
+                    && _sourceUnit == other._sourceUnit
+                    && _destinationUnit == other._destinationUnit
+                    && factor == other.factor
+        } else {
+            false
+        }
     }
 }
