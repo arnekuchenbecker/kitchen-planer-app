c818d20b1fae9c6a2526ccee1f81a7239ea719e5
Arne Kuchenbecker <arne.kuchenb@gmail.com>
2025-01-08 14:51:12 +0100

    Adjust database version

--- app/src/main/java/com/scouts/kitchenplaner/datalayer/KitchenAppDatabase.kt
+++ app/src/main/java/com/scouts/kitchenplaner/datalayer/KitchenAppDatabase.kt
@@ -64,7 +64,7 @@ import com.scouts.kitchenplaner.datalayer.typeconverters.DietaryTypeConverter
         StaticShoppingListEntryEntity::class,
         UserRecipeEntity::class
     ],
-    version = 9,
+    version = 10,
     exportSchema = false
 )
 @TypeConverters(DateConverter::class, DietaryTypeConverter::class)
